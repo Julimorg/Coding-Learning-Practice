e80164f066d7c3fdbb5841f2dfbf003f98122fdd
Tran Kien Phong <kienphongtran2003@gmail.com>
2024-11-14 10:48:38 +0700

    Java

---
+++ 300 bai code VN/src/JavaMasterClassCoursePractice/PlayingCat.java
@@ -0,0 +1,26 @@
+package JavaMasterClassCoursePractice;
+
+import java.util.Scanner;
+
+public class PlayingCat {
+
+    public static boolean isCatPlaying (boolean summer, int temperature)
+    {
+        if(summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            } else return false;
+        }else if (temperature >= 25 && temperature <= 35) {
+            return true;
+        }else return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("" +  isCatPlaying(false,a));
+    }
+}
+
---
+++ 300 bai code VN/src/JavaMasterClassCoursePractice/Practice3.java
@@ -0,0 +1,42 @@
+package JavaMasterClassCoursePractice;
+
+public class Practice3 {
+    public static int calculateScore (String playerName, int score )
+    {
+        System.out.print( "" + playerName + " " +score);
+        return score  * 1000;
+    }
+//**    Method Overloading
+    public static int calculateScore (int score)
+    {
+        System.out.print( "Unknowed player"  +score);
+        return score  * 1000;
+    }
+    public static String calculateScore(String playerName)
+    {
+//        System.out.print( "" + playerName);
+        return playerName;
+    }
+
+    public static int calculateScore ()
+    {
+        System.out.print("is Empty ");
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+            //? Method 1
+            int newScore = calculateScore("Harry Potter", 20);
+            System.out.print("\n");
+            System.out.println("Harry Potter is " + newScore);
+
+        //? Method 2
+            int newScore2 = calculateScore(40);
+        System.out.print("\n");
+        System.out.println("Harry Potter is " + newScore2);
+        calculateScore();
+        System.out.print("\n");
+        System.out.println(calculateScore("Fong"));
+    }
+}
