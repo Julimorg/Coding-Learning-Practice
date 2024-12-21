2c67a4d5be2d83e25011c8348515b1d92cf48611
Tran Kien Phong <kienphongtran2003@gmail.com>
2024-12-21 09:17:01 +0700

    0033 0034 0035

--- 300 bai code VN/src/bai0034.java
+++ 300 bai code VN/src/bai0034.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
 
-public class bai0033 {
+public class bai0034 {
     public static boolean checkCountPrime(int n)
     {
-        if ( n < 2 )
+        if ( n < 2)
         {
             return false;
         }
-        for(int i = 2 ; i <= Math.sqrt(n); i++)
+        for(int i = 2; i <= Math.sqrt(n); i++)
         {
             if ( n % i == 0)
             {
                 return false;
             }
+
         }
         return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
-        int count = 0;
-        for ( int i = m; i <= n; i++)
+        boolean hasPrime = false;
+        for(int i = m; i <= n; i++)
         {
-            if (checkCountPrime(i))
+            if(checkCountPrime(i))
             {
-                count++;
+                System.out.print(i + " ");
+                hasPrime = true;
             }
         }
-        System.out.println(count);
+        if (!hasPrime) {
+            System.out.print("-");
+        }
+
     }
 }
--- 300 bai code VN/src/bai0035.java
+++ 300 bai code VN/src/bai0035.java
@@ -1,33 +1,46 @@
 import java.util.Scanner;
 
-public class bai0033 {
+public class bai0035 {
+
     public static boolean checkCountPrime(int n)
     {
-        if ( n < 2 )
+        if ( n < 2)
         {
             return false;
         }
-        for(int i = 2 ; i <= Math.sqrt(n); i++)
+        for(int i = 2; i <= Math.sqrt(n); i++)
         {
             if ( n % i == 0)
             {
                 return false;
             }
+
         }
         return true;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
+        int sum = 0 ;
+        double avg = 0.00;
         int count = 0;
-        for ( int i = m; i <= n; i++)
+        boolean hasPrime = false;
+        for(int i = m; i <= n; i++)
         {
-            if (checkCountPrime(i))
+            if(checkCountPrime(i))
             {
                 count++;
+                avg  = ((double) (sum += i) / count);
+                hasPrime = true;
             }
         }
-        System.out.println(count);
+        if (count == 0) {
+            System.out.print("-");
+        } else {
+             avg = (double) sum / count;
+            System.out.printf("%.2f", avg);
+        }
     }
 }
