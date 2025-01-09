f59e2a0c92b7e9f76472434a718b2449ff4b7902
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-01-09 14:05:55 +0700

    1/9/2025

---
+++ 300 bai code VN/src/JavaMasterClassCoursePractice/OOP_Inheritance/OOP18/Rectangle03.java
@@ -0,0 +1,16 @@
+package JavaMasterClassCoursePractice.OOP_Inheritance.OOP18;
+
+public class Rectangle03 extends Shape{
+    private int width;
+    private int height;
+    public Rectangle03(int x, int y) {
+        this(x,y,0,0);
+    }
+
+    public Rectangle03(int x, int y, int width, int height) {
+        super(x, y);
+        this.width = width;
+        this.height = height;
+
+    }
+}
