52bb08cdaf1df20fc0018bddc4321587725f2b3e
Julimorg <kienphongtran2003@gmail.com>
2025-06-06 20:10:27 +0700

    .

---
+++ 300 bai code VN/src/Dependency/Company/Main.java
@@ -0,0 +1,9 @@
+package Dependency.Company;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager("Binh");
+        Employee employee = new Employee("An", manager);
+
+    }
+}
